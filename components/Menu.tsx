b1a9af03930e5683c1564066c1f138859bacd039
Nikhil Saraf <nsaraf98@gmail.com>
2020-07-13 20:29:56 +0530

    add logout button

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -13,12 +13,17 @@ import {
   PseudoBox,
   Button,
   CircularProgress,
+  DrawerFooter,
+  useToast,
 } from "@chakra-ui/core";
 import { Drawings, gql, useSubscription } from "magiql";
 import React from "react";
-import { SlDesignToolPens, SlPencil2 } from "react-icons/sl";
+import { SlDesignToolPens, SlPencil2, SlLogout2 } from "react-icons/sl";
 import { useStudio, useCreateNewDrawing } from "./StudioContext";
 import { useSession } from "./Session";
+import { EXCALIDRAW_USER_NAME_KEY } from "pages/login";
+import { HASURA_ADMIN_SECRET_KEY } from "./getCookieToken";
+import { useRouter } from "next/router";
 
 const DrawingItem = ({
   drawing,
@@ -44,6 +49,7 @@ const DrawingItem = ({
           changeDrawingId(drawing.id);
           closeDrawer();
         }}
+        cursor="pointer"
         display="flex"
         alignItems="center"
         justifyContent="space-between"
@@ -66,24 +72,11 @@ export function DrawingsMenu() {
     drawerButtonRef,
   } = useStudio();
 
-  const createDrawing = useCreateNewDrawing();
+  // const [refresh, setRefresh] = React.useState(0);
 
-  const { data, isSuccess, isLoading } = useSubscription<
-    { drawings: Drawings[] },
-    {},
-    Error
-  >(gql`
-    subscription liveDrawings {
-      drawings(order_by: { created_at: desc }) {
-        collaboration_link
-        file_id
-        id
-        last_edited
-        name
-        is_live
-      }
-    }
-  `);
+  const createDrawing = useCreateNewDrawing();
+  const router = useRouter();
+
   return (
     <Drawer
       isOpen={isOpen}
@@ -96,7 +89,10 @@ export function DrawingsMenu() {
         <DrawerCloseButton />
         <DrawerHeader mt={6}>
           <Flex alignItems="center" ml={-4}>
-            <Image src="/apple-touch-icon.png" height={[12, 12, 16]} />
+            <Text fontSize={[12, 12, 48]} mx={3}>
+              📋
+            </Text>
+            {/* <Image src="/apple-touch-icon.png" height={[12, 12, 16]} /> */}
             <Flex direction="column">
               <Text
                 fontFamily="Virgil"
@@ -112,36 +108,122 @@ export function DrawingsMenu() {
               </Text>
             </Flex>
           </Flex>
-        </DrawerHeader>
-        <DrawerBody>
-          {isLoading && (
-            <Flex width="100%" justifyContent="center">
-              <CircularProgress capIsRound isIndeterminate color="gray" />
-            </Flex>
-          )}
-          {isSuccess && (
-            <>
-              <Divider m={0} />
-              {data?.drawings.map((drawing: any) => (
-                <React.Fragment key={drawing.id}>
-                  <DrawingItem drawing={drawing} closeDrawer={onClose} />
-                  <Divider m={0} />
-                </React.Fragment>
-              ))}
-            </>
-          )}
+
           <Button
             mt={6}
             width="100%"
+            variant="outline"
+            justifyContent="flex-start"
             color="gray.500"
+            fontSize={["sm", "sm", "md", "md"]}
+          >
+            🧑‍🎨{" "}
+            <Text ml={3}>
+              {typeof window !== "undefined"
+                ? window.localStorage.getItem(EXCALIDRAW_USER_NAME_KEY) ||
+                  "Excalidraw User"
+                : "Excalidraw User"}
+            </Text>
+          </Button>
+        </DrawerHeader>
+        <DrawerBody>
+          <DrawingsList closeDrawer={onClose} />
+          <Button
+            mt={6}
+            width="100%"
+            variantColor="blue"
             fontSize={["sm", "sm", "md", "md"]}
             onClick={createDrawing}
           >
             <SlDesignToolPens style={{ marginRight: 8 }} />
             Create New Drawing
           </Button>
+          {/* <Button
+            mt={3}
+            width="100%"
+            variantColor="gray"
+            fontSize={["sm", "sm", "md", "md"]}
+            onClick={() => setRefresh((r) => r + 1)}
+          >
+            <SlButtonRefresh style={{ marginRight: 8 }} />
+            Refresh
+          </Button> */}
         </DrawerBody>
+        <DrawerFooter>
+          <Button
+            mt={6}
+            variantColor="gray"
+            width="100%"
+            // color="gray.500"
+            fontSize={["sm", "sm", "md", "md"]}
+            onClick={() => {
+              window.localStorage.removeItem(HASURA_ADMIN_SECRET_KEY);
+              window.localStorage.removeItem(EXCALIDRAW_USER_NAME_KEY);
+              router.replace("/login");
+            }}
+          >
+            <SlLogout2 style={{ marginRight: 8 }} />
+            Logout
+          </Button>
+        </DrawerFooter>
       </DrawerContent>
     </Drawer>
+  );
+}
+
+function DrawingsList({ closeDrawer }: any) {
+  const toast = useToast();
+
+  const { data, isSuccess, isLoading } = useSubscription<
+    { drawings: Drawings[] },
+    {},
+    Error
+  >(
+    gql`
+      subscription liveDrawings {
+        drawings(order_by: { created_at: desc }) {
+          collaboration_link
+          file_id
+          id
+          last_edited
+          name
+          is_live
+        }
+      }
+    `,
+    {
+      onError: (e: any) => {
+        toast({
+          title: "Server error",
+          description:
+            e?.response?.errors
+              ?.map((err: any) => err.extensions.code + ": " + err.message)
+              .join("\n") ?? JSON.stringify(e),
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      },
+    }
+  );
+  return (
+    <>
+      {isLoading && (
+        <Flex width="100%" justifyContent="center">
+          <CircularProgress capIsRound isIndeterminate color="gray" />
+        </Flex>
+      )}
+      {isSuccess && (
+        <>
+          <Divider m={0} />
+          {data?.drawings.map((drawing: any) => (
+            <React.Fragment key={drawing.id}>
+              <DrawingItem drawing={drawing} closeDrawer={closeDrawer} />
+              <Divider m={0} />
+            </React.Fragment>
+          ))}
+        </>
+      )}
+    </>
   );
 }
